478fdb3de8c16843b453c751db2be34466e23e70
ashif1996 <ashifhassan6666@gmail.com>
2024-10-18 18:54:23 +0530

    Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.use((err, req, res, next) => {
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} smoothly.`);
 });
 
 module.exports = app;
