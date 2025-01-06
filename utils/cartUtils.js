4d6cd87b7a29b5ba9ca3759df9c118e819504308
Ashif Hassan <ashifhassan1996@gmail.com>
2025-01-06 14:41:59 +0530

    update cart utils

--- utils/cartUtils.js
+++ utils/cartUtils.js
@@ -7,37 +7,25 @@ const calculateSubTotal = (cart) => {
 
 const calculateCouponDiscount = (coupon, subTotal) => {
     if (!coupon) return 0;
-    return coupon.type === 'PERCENTAGE' ? subTotal * (coupon.discountValue / 100) : coupon.discountValue;
+    return coupon.type === 'PERCENTAGE'
+        ? subTotal * (coupon.discountValue / 100)
+        : coupon.discountValue;
 };
 
 const recalculateCartTotals = (cart) => {
     try {
-        // Calculate subTotal
         cart.subTotal = calculateSubTotal(cart);
-
-        // Calculate coupon discount
         cart.couponDiscount = calculateCouponDiscount(cart.couponId, cart.subTotal);
-
-        // Set itemCount
         cart.itemCount = cart.items.length;
 
-        // Debugging: Log intermediate values
-        console.log('SubTotal:', cart.subTotal);
-        console.log('Coupon Discount:', cart.couponDiscount);
-        console.log('Shipping Charge:', cart.shippingCharge);
-
-        // Calculate totalPrice
         cart.totalPrice = (cart.subTotal - cart.couponDiscount + cart.shippingCharge) || 0;
 
-        // Debugging: Log totalPrice
-        console.log('Calculated Total Price:', cart.totalPrice);
-
-        // Ensure all values are numbers
         cart.subTotal = Number(cart.subTotal);
         cart.couponDiscount = Number(cart.couponDiscount);
         cart.totalPrice = Number(cart.totalPrice);
-    } catch (err) {
-        console.error('Error recalculating cart totals: ', err);
+    } catch (error) {
+        console.error('Error recalculating cart totals: ', error);
+        throw new Error("An error occurred. Please try again later.");
     }
 };
 
@@ -45,5 +33,5 @@ const recalculateCartTotals = (cart) => {
 module.exports = {
     calculateSubTotal,
     calculateCouponDiscount,
-    recalculateCartTotals
-}
+    recalculateCartTotals,
+};
