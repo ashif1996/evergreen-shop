ada4a19e37deccde1d0e813ac8996982188905c3
Ashif Hassan <ashifhassan1996@gmail.com>
2025-01-06 18:39:31 +0530

    update user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
 const bcrypt = require("bcrypt");
+
 const Address = require("../models/addressSchema");
 const Cart = require("../models/cartSchema");
 const Product = require("../models/product");
 const User = require("../models/user");
 const Wishlist = require("../models/wishlistSchema");
+
 const { calculateBestDiscountedPrice } = require("../utils/discountPriceCalculation");
 const {
   creditReferralReward,
@@ -18,14 +20,14 @@ const HttpStatus = require("../utils/httpStatus");
 const getUserSignup = (req, res) => {
   const locals = { title: "Sign up | EverGreen" };
 
-  return res.render("users/signup", {
+  res.render("users/signup", {
     locals,
     layout: "layouts/authLayout",
   });
 };
 
 // Handles user signup process
-const userSignup = async (req, res, next) => {
+const userSignup = async (req, res) => {
   const { firstName, lastName, referralCode, email, password } = req.body;
 
   try {
@@ -41,13 +43,13 @@ const userSignup = async (req, res, next) => {
       referrer = validationResult.referrer;
     }
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.exists({ email });
     if (existingUser) {
       return errorHandler(res, HttpStatus.BAD_REQUEST, "User already exists.");
     }
 
     // Create new user
-    const newUser = new User({
+    const newUser = await User.create({
       firstName,
       lastName,
       email,
@@ -55,19 +57,18 @@ const userSignup = async (req, res, next) => {
       referredBy: referrer ? referrer._id : null,
     });
 
-    const user = await newUser.save();
     const generatedReferralCode = await generateUniqueReferralCode();
-    user.referralCode = generatedReferralCode;
-    await user.save();
+    newUser.referralCode = generatedReferralCode;
+    await newUser.save();
 
     if (referrer) {
-      await creditReferralReward(referrer._id, user);
+      await creditReferralReward(referrer._id, newUser);
     }
 
     return successHandler(res, HttpStatus.CREATED, "You have registered successfully.");
-  } catch (err) {
-    console.error("Error registering the user: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error registering the user: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
@@ -129,9 +130,9 @@ const userLogin = async (req, res, next) => {
     };
 
     return res.redirect("/");
-  } catch (err) {
-    console.error("An error occurred during login: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("An error occurred during login: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
@@ -141,7 +142,7 @@ const getForgotPassword = (req, res) => {
     title: `Forgotten Password | Can't Log In | Request OTP | EverGreen`,
   };
 
-  return res.render("users/forgot-password", {
+  res.render("users/forgot-password", {
     locals,
     layout: "layouts/authLayout",
   });
@@ -151,14 +152,14 @@ const getForgotPassword = (req, res) => {
 const getChangePassword = (req, res) => {
   const locals = { title: `Change Password | Request OTP | EverGreen` };
 
-  return res.render("users/change-password", {
+  res.render("users/change-password", {
     locals,
     layout: "layouts/authLayout",
   });
 };
 
 // Retrieves and renders the user profile page
-const getUserProfile = async (req, res, next) => {
+const getUserProfile = async (req, res) => {
   const locals = {
     title: "User Profile | EverGreen",
     user: null,
@@ -168,7 +169,10 @@ const getUserProfile = async (req, res, next) => {
   const userId = req.session.user._id;
 
   try {
-    const user = await User.findById(userId).populate("addresses");
+    const user = await User.findById(userId)
+      .populate("addresses")
+      .lean();
+
     if (!user) {
       const errorMessage = "User not found. Try again using another account.";
       return res.redirect(`/error?statusCode=404&errorMessage=${encodeURIComponent(errorMessage)}`);
@@ -176,14 +180,14 @@ const getUserProfile = async (req, res, next) => {
 
     locals.user = user;
 
-    return res.render("users/profile", {
+    res.render("users/profile", {
       locals,
       addresses: user.addresses || [],
       layout: "layouts/userLayout",
     });
-  } catch (err) {
-    console.error("Error fetching user profile: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error fetching user profile: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
@@ -195,35 +199,35 @@ const getEditProfile = (req, res) => {
     isLoggedIn: !!req.session.user,
   };
 
-  return res.render("users/editProfile", {
+  res.render("users/editProfile", {
     locals,
     layout: "layouts/userLayout",
   });
 };
 
 // Handles the editing of user profile
-const editProfile = async (req, res, next) => {
+const editProfile = async (req, res) => {
   const userId = req.session.user._id;
   const { firstName, lastName } = req.body;
 
   try {
-    await User.findByIdAndUpdate(userId, {
-      firstName,
-      lastName,
-    });
+    await User.findByIdAndUpdate(
+      userId,
+      { firstName, lastName },
+    );
 
     req.session.user.firstName = firstName;
     req.session.user.lastName = lastName;
 
     return successHandler(res, HttpStatus.OK, "Profile updated successfully.");
-  } catch (err) {
-    console.error("Error updating user profile: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error updating user profile: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Renders the address management page for the logged-in user
-const getAddressManagement = async (req, res, next) => {
+const getAddressManagement = async (req, res) => {
   const locals = {
     title: "Address Management | EverGreen",
     user: req.session.user,
@@ -233,22 +237,24 @@ const getAddressManagement = async (req, res, next) => {
 
   try {
     const userId = req.session.user._id;
-    const user = await User.findById(userId).populate("addresses");
+    const user = await User.findById(userId)
+      .populate("addresses")
+      .lean();
 
     locals.addresses = user.addresses;
 
-    return res.render("users/addressManagement", {
+    res.render("users/addressManagement", {
       locals,
       layout: "layouts/userLayout",
     });
-  } catch (err) {
-    console.error("Error fetching address management: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error fetching address management: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Adds or updates a user address
-const addAddress = async (req, res, next) => {
+const addAddress = async (req, res) => {
   const { addressId, address, city, state, zipCode } = req.body;
   const userId = req.session.user._id;
 
@@ -272,7 +278,7 @@ const addAddress = async (req, res, next) => {
       return successHandler(res, HttpStatus.OK, "Address updated successfully.");
     } else {
       // Create and save new address
-      const newAddress = new Address({
+      const newAddress = await Address.create({
         userId,
         address,
         city,
@@ -280,20 +286,19 @@ const addAddress = async (req, res, next) => {
         zipCode,
       });
 
-      await newAddress.save();
       user.addresses.push(newAddress._id);
       await user.save();
 
       return successHandler(res, HttpStatus.OK, "Address added successfully.");
     }
-  } catch (err) {
-    console.error("Error adding or updating address: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error adding or updating address: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Deletes a user's address
-const deleteAddress = async (req, res, next) => {
+const deleteAddress = async (req, res) => {
   const { addressId } = req.body;
   const userId = req.session.user._id;
 
@@ -313,14 +318,14 @@ const deleteAddress = async (req, res, next) => {
     await Address.findByIdAndDelete(addressId);
 
     return successHandler(res, HttpStatus.OK, "Address deleted successfully.");
-  } catch (err) {
-    console.error("Error deleting the address: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error deleting the address: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Retrieves and displays the user's shopping cart
-const getShoppingCart = async (req, res, next) => {
+const getShoppingCart = async (req, res) => {
   const locals = {
     title: "Shopping Cart | EverGreen",
     user: req.session.user,
@@ -340,14 +345,13 @@ const getShoppingCart = async (req, res, next) => {
     });
 
     if (!cart) {
-      cart = new Cart({
+      cart = await Cart.create({
         userId,
         items: [],
         subTotal: 0,
         shippingCharge: 30,
         totalPrice: 30,
       });
-      await cart.save();
     }
 
     // Only set the cart reference if it was newly created
@@ -365,20 +369,20 @@ const getShoppingCart = async (req, res, next) => {
       item.discountPercentage = discountDetails.discountPercentage;
     });
 
-    return res.render("users/cart", {
+    res.render("users/cart", {
       locals,
       user: user,
       cart: cart,
       layout: "layouts/userLayout",
     });
-  } catch (err) {
-    console.error("Error fetching cart: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error fetching cart: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Adds a product to the user's shopping cart
-const addProduct = async (req, res, next) => {
+const addProduct = async (req, res) => {
   try {
     const userId = req.session.user._id;
     const user = await User.findById(userId);
@@ -400,14 +404,14 @@ const addProduct = async (req, res, next) => {
 
     let cart = await Cart.findOne({ userId });
     if (!cart) {
-      cart = new Cart({
+      cart = await Cart.create({
         userId,
         items: [],
         subTotal: 0,
         shippingCharge: 30,
         totalPrice: 30,
       });
-      await cart.save()
+
       user.cart = cart._id;
       await user.save();
     }
@@ -444,14 +448,14 @@ const addProduct = async (req, res, next) => {
       cart,
       itemCount,
     });
-  } catch (err) {
-    console.error("Error adding product to cart: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error adding product to cart: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Updates the quantity of a product in the user's shopping cart
-const updateCartQuantity = async (req, res, next) => {
+const updateCartQuantity = async (req, res) => {
   const { productId, quantity } = req.body;
   const userId = req.session.user._id;
 
@@ -489,14 +493,14 @@ const updateCartQuantity = async (req, res, next) => {
       return errorHandler(res, HttpStatus.NOT_FOUND, "Item not found in cart.");
     }
     return errorHandler(res, HttpStatus.NOT_FOUND, "Cart not found.");
-  } catch (err) {
-    console.error("Error updating cart quantity: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error updating cart quantity: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Deletes a product from the user's shopping cart
-const deleteCartItems = async (req, res, next) => {
+const deleteCartItems = async (req, res) => {
   const { productId } = req.body;
   const userId = req.session.user._id;
 
@@ -531,14 +535,14 @@ const deleteCartItems = async (req, res, next) => {
         message: "Product removed from your cart.",
       });
     }
-  } catch (err) {
-    console.error("Error deleting cart item: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error deleting cart item: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Fetches the user's wishlist and handles pagination
-const getWishlist = async (req, res, next) => {
+const getWishlist = async (req, res) => {
   const locals = {
     title: "Shopping Cart | EverGreen",
     user: req.session.user,
@@ -551,11 +555,10 @@ const getWishlist = async (req, res, next) => {
 
     let wishlist = await Wishlist.findOne({ userId }).populate("items.productId");
     if (!wishlist) {
-      wishlist = new Wishlist({
+      wishlist = await Wishlist.create({
         userId,
         items: [],
       });
-      await wishlist.save();
     }
 
     // Only set the wishlist reference if it was newly created
@@ -570,7 +573,7 @@ const getWishlist = async (req, res, next) => {
     const totalPages = Math.ceil(totalItems / limit);
     const paginatedItems = wishlist.items.slice((page - 1) * limit, page * limit);
 
-    return res.render("users/wishlist", {
+    res.render("users/wishlist", {
       locals,
       wishlist: { ...wishlist, items: paginatedItems },
       currentPage: page,
@@ -579,12 +582,12 @@ const getWishlist = async (req, res, next) => {
     });
   } catch (error) {
     console.error("Error fetching Wishlist: ", error);
-    return next(err);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Adds a product to the user's wishlist
-const addToWishlist = async (req, res, next) => {
+const addToWishlist = async (req, res) => {
   try {
     const userId = req.session.user._id;
     const user = await User.findById(userId);
@@ -601,11 +604,11 @@ const addToWishlist = async (req, res, next) => {
 
     let wishlist = await Wishlist.findOne({ userId });
     if (!wishlist) {
-      wishlist = new Wishlist({
+      wishlist = await Wishlist.create({
         userId,
         items: [],
       });
-      await wishlist.save();
+
       user.wishlist = wishlist._id;
       await user.save();
     }
@@ -619,14 +622,14 @@ const addToWishlist = async (req, res, next) => {
     await wishlist.save();
 
     return successHandler(res, HttpStatus.OK, `Product added to your wishlist.`);
-  } catch (err) {
-    console.error("Error adding product to wishlist: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error adding product to wishlist: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Deletes a product from the user's wishlist
-const deleteWishlistItems = async (req, res, next) => {
+const deleteWishlistItems = async (req, res) => {
   try {
     const userId = req.session.user._id;
     const { productId } = req.body;
@@ -653,14 +656,14 @@ const deleteWishlistItems = async (req, res, next) => {
 
       return successHandler(res, HttpStatus.OK, `${product.name} is removed from the wishlist.`);
     }
-  } catch (err) {
-    console.error("Error deleting wishlist item: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error deleting wishlist item: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Fetches the referrals of the logged-in user
-const getReferrals = async (req, res, next) => {
+const getReferrals = async (req, res) => {
   const locals = {
     title: "Shopping Cart | EverGreen",
     user: req.session.user,
@@ -676,26 +679,27 @@ const getReferrals = async (req, res, next) => {
 
     const referrals = await User.find({ referredBy: user._id });
 
-    return res.render("users/referrals", {
+    res.render("users/referrals", {
       locals,
       user: user,
       referrals: referrals,
       layout: "layouts/userLayout",
       formatDate: (date) => new Date(date).toLocaleDateString(),
     });
-  } catch (err) {
-    console.error("Error fetching referral data: ", err);
-    return next(err);
+  } catch (error) {
+    console.error("Error fetching referral data: ", error);
+    throw new Error("An error occurred. Please try again later.");
   }
 };
 
 // Logs out the user and redirects to the login page
-const userLogout = (req, res, next) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.error("Error destroying the session: ", err);
-      return next(err);
+const userLogout = (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.error("Error destroying the session: ", error);
+      throw new Error("An error occurred. Please try again later.");
     }
+    
     return res.redirect("/users/login");
   });
 };
