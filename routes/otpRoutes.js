b267ab33dcc8c1d7e63057bc1ce453bcd2d83b40
Ashif Hassan <ashifhassan1996@gmail.com>
2025-01-06 18:40:37 +0530

    update otp routes

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -9,10 +9,10 @@ const validate = require('../middlewares/validate');
 // Routes for OTP and password reset functionalities
 router.post('/send-otp', otpEmailValidationRules, validate, otpController.handleSendOtp);
 
-router.route("verify-otp")
+router.route("/verify-otp")
     .get(otpController.getOtpVerification)
     .post(otpValidationRules, validate, otpController.handleVerifyOtp);
-    
+
 router.route("/reset-password")
     .get(otpController.getResetPassword)
     .post(resetPasswordValidationRules, validate, otpController.handleResetPassword);
